6aeff4ba36e0cc731e2376d041d56c046c286ba4
dmitry-skrypka <dimitry.skripka@gmail.com>
2018-10-26 16:07:58 +0300

    Updates

---
+++ precache-manifest.e375414260642ea089509816321fcca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ig-client-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "698991835a57b4cf2f6d",
+    "url": "/ig-client-list/static/js/runtime~main.69899183.js"
+  },
+  {
+    "revision": "c4c6671d26f012ffa56f",
+    "url": "/ig-client-list/static/js/main.c4c6671d.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/ig-client-list/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "c4c6671d26f012ffa56f",
+    "url": "/ig-client-list/static/css/main.57411656.chunk.css"
+  },
+  {
+    "revision": "5335d3ff09b1233155c45567fe6563e6",
+    "url": "/ig-client-list/index.html"
+  }
+];
