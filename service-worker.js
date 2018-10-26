111506d8e8d3678bd6f156d0f3ae93cd276f98bf
dmitry-skrypka <dimitry.skripka@gmail.com>
2018-10-27 00:40:43 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/ig-client-list/precache-manifest.e375414260642ea089509816321fcca7.js"
+  "/ig-client-list/precache-manifest.d672856f592c40cef6c1e01999081afd.js"
 );
 
 workbox.clientsClaim();
